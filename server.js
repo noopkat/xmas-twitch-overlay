e0e66eb573e1679087851c2670dcf2b3631e0b43
Suz Hinton <suz.hinton@gmail.com>
2017-12-25 08:52:14 +1100

    commit live streamed code

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const IRC = require('irc-framework');
+const skateboard = require('skateboard');
+
+const bot = new IRC.Client();
+let count = 0;
+let socket = null;
+const queue = [];
+
+function botConnect(callback) {
+  bot.connect({
+    host: 'irc.chat.twitch.tv',
+    port: 6667,
+    nick: process.env.TWITCH_NICK,
+    password: process.env.TWITCH_TOKEN
+  });
+
+  bot.on('registered', () => {
+    console.log('reg happened!');
+
+    const channel = bot.channel('#noopkat');
+    channel.join();
+    return callback();
+  });
+};
+
+function dequeue() {
+  if (queue.length) {
+    if (socket) {
+      const nick = queue.shift();
+      socket.write(nick); 
+    }
+  }
+}
+
+botConnect(() => {
+  skateboard({}, (stream) => {
+    socket = stream;
+  });
+  
+  bot.matchMessage(/merry christmas|happy holidays/i, (event) => {
+    console.log(event);
+    queue.push(event.nick);
+  });
+  
+  const queueInterval = setInterval(dequeue, 5500);
+});
